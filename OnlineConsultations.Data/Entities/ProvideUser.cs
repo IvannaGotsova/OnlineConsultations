85b60d38c1f6a9c091e142baf1284752c58104d1
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-02 17:57:16 +0200

    Added properties

--- OnlineConsultations.Data/Entities/ProvideUser.cs
+++ OnlineConsultations.Data/Entities/ProvideUser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,19 @@ namespace OnlineConsultations.Data.Entities
 {
     internal class ProvideUser : ApplicationUser
     {
+
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; } = null!;
+
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser? ApplicationUser { get; set; }
+
+        public IEnumerable<Answer> Answers = new List<Answer>();
+        public Post? Post { get; set; } 
+
+        public IEnumerable<Message> Messages = new List<Message>();
     }
 }
