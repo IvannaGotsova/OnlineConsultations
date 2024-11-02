38a82368aabf67acd779a526f17e36cafbd37095
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-03 01:14:05 +0200

    Added reference

---
+++ OnlineConsultations.Data/OnlineConsultationsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineConsultations.Data
+{
+    public class OnlineConsultationsDbContext : IdentityDbContext
+    {
+        public OnlineConsultationsDbContext(DbContextOptions<OnlineConsultationsDbContext> options)
+            : base(options)
+        {
+           
+        }
+    }
+}
