fe028e283524ee708d1a1afa471bdbc609470087
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-02 12:21:49 +0200

    Added basic ApplicationUser entity

---
+++ OnlineConsultations.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.ApplicationUserConstants;
+
+namespace OnlineConsultations.Data.Entities
+{
+    internal class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(UserMaxLengthFirstName)]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        [StringLength(UserMaxLengthLastName)]
+        public string LastName { get; set; } = null!;
+    }
+}
