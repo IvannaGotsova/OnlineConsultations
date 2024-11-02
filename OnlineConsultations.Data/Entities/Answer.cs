6574c82ff00c5f3046ae7ff614e50b8af2c57320
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-03 00:53:09 +0200

    Added attributes - key and max length of string

--- OnlineConsultations.Data/Entities/Answer.cs
+++ OnlineConsultations.Data/Entities/Answer.cs
@@ -5,15 +5,18 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.AnswerConstants;
 
 namespace OnlineConsultations.Data.Entities
 {
     internal class Answer
     {
+        [Key]
         [Required]
         public int AnswerId { get; set; }
 
         [Required]
+        [StringLength(AnswerDescriptionMaxLength)]
         public required string AnswerDescription{ get; set; }
 
         [Required]
