45262944a42d873d669c7d2075250e1c05ad5328
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-02 17:57:44 +0200

    Added properties

--- OnlineConsultations.Data/Entities/SearchUser.cs
+++ OnlineConsultations.Data/Entities/SearchUser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,23 @@ namespace OnlineConsultations.Data.Entities
 {
     internal class SearchUser : ApplicationUser
     {
+        [Required]
+        public int SearchUserId { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; } = null!;
+
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser? ApplicationUser { get; set; }
+
+        public IEnumerable<Question> Questions = new List<Question>();
+
+        public IEnumerable<Review> Reviews = new List<Review>();
+
+        public IEnumerable<Rating> Ratings = new List<Rating>();
+
+        public IEnumerable<Comment> Comments = new List<Comment>();
+
+        public IEnumerable<Message> Messages = new List<Message>();
     }
 }
