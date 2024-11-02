a777ae14b6b9114e6729d25f6615b014d0928fb4
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-03 01:02:25 +0200

    Added attributes - key and max length of string

--- OnlineConsultations.Data/Entities/Post.cs
+++ OnlineConsultations.Data/Entities/Post.cs
@@ -5,18 +5,22 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.PostConstants;
 
 namespace OnlineConsultations.Data.Entities
 {
     internal class Post
     {
+        [Key]
         [Required]
         public int PostId { get; set; }
 
         [Required]
+        [StringLength(PostTitleMaxLength)]
         public required string PostTitle { get; set; }
 
         [Required]
+        [StringLength(PostDescriptionMaxLength)]
         public required string PostDescription { get; set; }
 
         [Required]
