a699a8c51c64d65ad09724c9846a1ca1a9f1c7c8
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-02 17:57:09 +0200

    Added properties

--- OnlineConsultations.Data/Entities/Post.cs
+++ OnlineConsultations.Data/Entities/Post.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,22 @@ namespace OnlineConsultations.Data.Entities
 {
     internal class Post
     {
+        [Required]
+        public int PostId { get; set; }
+
+        [Required]
+        public required string PostTitle { get; set; }
+
+        [Required]
+        public required string PostDescription { get; set; }
+
+        [Required]
+        public required decimal PostPayPerDay { get; set; }
+
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public required ProvideUser ProvideUser { get; set; }
     }
 }
