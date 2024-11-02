672a57f24930a3324dba2cbdce8e7e6bdf579a29
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-02 17:57:30 +0200

    Added properties

--- OnlineConsultations.Data/Entities/Rating.cs
+++ OnlineConsultations.Data/Entities/Rating.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,22 @@ namespace OnlineConsultations.Data.Entities
 {
     internal class Rating
     {
+        [Required]
+        public int RatingId { get; set; }
+
+        [Required]
+        public int RatingValue { get; set; }
+
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public ProvideUser ProvideUser { get; set; } = null!;
+
+        [Required]
+        public int SearchUserId { get; set; }
+
+        [ForeignKey(nameof(SearchUserId))]
+        public SearchUser SearchUser { get; set; } = null!;
     }
 }
