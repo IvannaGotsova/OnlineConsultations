1e9fc0c6d4a46f3537402d52273c22e1268b6041
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-02 17:57:40 +0200

    Added properties

--- OnlineConsultations.Data/Entities/Review.cs
+++ OnlineConsultations.Data/Entities/Review.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,25 @@ namespace OnlineConsultations.Data.Entities
 {
     internal class Review
     {
+        [Required]
+        public int ReviewId { get; set; }
+
+        [Required]
+        public string? ReviewDescription { get; set; }
+
+        [Required]
+        public DateTime ReviewDate { get; set; }
+
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public ProvideUser ProvideUser { get; set; } = null!;
+
+        [Required]
+        public int SearchUserId { get; set; }
+
+        [ForeignKey(nameof(SearchUserId))]
+        public SearchUser SearchUser { get; set; } = null!;
     }
 }
