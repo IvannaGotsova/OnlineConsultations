c39e102bebe59d7452dc81fffc22fc8643d67faa
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-03 00:51:28 +0200

    Added constants for all entities

--- OnlineConsultations.Data/DataConstants/DataConstants.cs
+++ OnlineConsultations.Data/DataConstants/DataConstants.cs
@@ -8,6 +8,13 @@ namespace OnlineConsultations.Data.DataConstants
 {
     internal class DataConstants
     {
+        public class AnswerConstants
+        {
+            public const int AnswerDescriptionMinLength = 5;
+            public const int AnswerDescriptionMaxLength = 200000;
+
+        }
+
         public class ApplicationUserConstants
         {
             public const int UserMinLengthUsername = 5;
@@ -24,6 +31,53 @@ namespace OnlineConsultations.Data.DataConstants
 
             public const int UserMinLengthLastName = 5;
             public const int UserMaxLengthLastName = 20;
+        }
+
+        public class CommentConstants
+        {
+            public const int CommentTitleMinLength = 5;
+            public const int CommentTitleMaxLength = 50;
+
+            public const int CommentDescriptionMinLength = 5;
+            public const int CommentDescriptionMaxLength = 200000;
+        }
+
+        public class MessageConstants
+        {
+            public const int MessageDescriptionMinLength = 5;
+            public const int MessageDescriptionMaxLength = 200000;
+
+        }
+
+        public class PostConstants
+        {
+            public const int PostTitleMinLength = 5;
+            public const int PostTitleMaxLength = 20;
+                             
+            public const int PostDescriptionMinLength = 5;
+            public const int PostDescriptionMaxLength = 200000;
+        }
+
+        public class QuestionConstants
+        {
+            public const int QuestionTitleMinLength = 5;
+            public const int QuestionTitleMaxLength = 20;
+
+            public const int QuestionDescriptionMinLength = 5;
+            public const int QuestionDescriptionMaxLength = 200000;
+        }
+
+        public class RatingConstants
+        {
+            public const int RatingValueMinLength = 1;
+            public const int RatingValueMaxLength = 10;
+        }
+
+        public class ReviewConstants
+        {
+            public const int ReviewDescriptionMinLength = 5;
+            public const int ReviewDescriptionMaxLength = 200000;
+
         }
     }
 }
