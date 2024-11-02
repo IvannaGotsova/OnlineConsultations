3097b4ab7d4cf5aec0508c57461039f41fb492b3
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-02 17:56:15 +0200

    Added properties

--- OnlineConsultations.Data/Entities/Answer.cs
+++ OnlineConsultations.Data/Entities/Answer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,27 @@ namespace OnlineConsultations.Data.Entities
 {
     internal class Answer
     {
+        [Required]
+        public int AnswerId { get; set; }
+
+        [Required]
+        public required string AnswerDescription{ get; set; }
+
+        [Required]
+        public DateTime AnswerDate { get; set; }
+
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public ProvideUser ProvideUser { get; set; } = null!;
+
+        [Required]
+        public int QuestionId { get; set; }
+
+        [ForeignKey(nameof(QuestionId))]
+        public Question Question { get; set; } = null!;
+
+        public IEnumerable<Comment> Comments = new List<Comment>();
     }
 }
