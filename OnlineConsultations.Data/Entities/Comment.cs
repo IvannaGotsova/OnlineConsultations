61f7fb0230c729308da40890916d5f434b3932ea
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-02 17:56:30 +0200

    Added properties

--- OnlineConsultations.Data/Entities/Comment.cs
+++ OnlineConsultations.Data/Entities/Comment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,27 @@ namespace OnlineConsultations.Data.Entities
 {
     internal class Comment
     {
+        [Required]
+        public int CommentId { get; set; }
+
+        [Required]
+        public string? CommentTitle { get; set; }
+
+        [Required]
+        public string? CommentDescription { get; set; }
+
+        [Required]
+        public int GuestUserId { get; set; }
+
+        [ForeignKey(nameof(GuestUserId))]
+        public GuestUser GuestUser { get; set; } = null!;
+
+        [Required]
+        public int SearchUserId { get; set; }
+
+        [ForeignKey(nameof(SearchUserId))]
+        public SearchUser SearchUser { get; set; } = null!;
+
+        public Answer? Answer { get; set; }
     }
 }
