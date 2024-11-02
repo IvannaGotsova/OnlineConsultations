801b782eefd0a050e8b06e87d10a64c5a7dcb123
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-03 01:25:40 +0200

    Created DBSets in DBContext

--- OnlineConsultations.Data/OnlineConsultationsDbContext.cs
+++ OnlineConsultations.Data/OnlineConsultationsDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OnlineConsultations.Data.Entities;
 
 namespace OnlineConsultations.Data
 {
@@ -8,7 +9,28 @@ namespace OnlineConsultations.Data
         public OnlineConsultationsDbContext(DbContextOptions<OnlineConsultationsDbContext> options)
             : base(options)
         {
-           
+            if (this.Database.IsRelational())
+            {
+                this.Database.Migrate();
+            }
+            else
+            {
+                this.Database.EnsureCreated();
+                //this.seedDb = seed;
+            }
+
         }
+        public DbSet<ApplicationRole> ApplicationRoles { get; set; } = null!;
+        public DbSet<Answer> Answers { get; set; } = null!;
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; } = null!;
+        public DbSet<Comment> Comments { get; set; } = null!;
+        public DbSet<GuestUser> GuestUsers { get; set; } = null!;
+        public DbSet<Message> Messages { get; set; } = null!;
+        public DbSet<Post> Posts { get; set; } = null!;
+        public DbSet<ProvideUser> ProvideUsers { get; set; } = null!;
+        public DbSet<Question> Questions { get; set; } = null!;
+        public DbSet<Rating> Ratings { get; set; } = null!;
+        public DbSet<Review> Reviews { get; set; } = null!;
+        public DbSet<SearchUser> SearchUsers { get; set; } = null!;
     }
 }
