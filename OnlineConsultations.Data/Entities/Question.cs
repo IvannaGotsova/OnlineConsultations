d9771624a68bd67896a28571023c31e6d8483206
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-02 17:57:25 +0200

    Added properties

--- OnlineConsultations.Data/Entities/Question.cs
+++ OnlineConsultations.Data/Entities/Question.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,30 @@ namespace OnlineConsultations.Data.Entities
 {
     internal class Question
     {
+        [Required]
+        public int QuestionId { get; set; }
+
+        [Required]
+        public string? QuestionTitle { get; set; }
+
+        [Required]
+        public string? QuestionDescription { get; set; }
+
+        [Required]
+        public DateTime QuestionDate { get; set; }
+
+        [Required]
+        public int GuestUserId { get; set; }
+
+        [ForeignKey(nameof(GuestUserId))]
+        public GuestUser GuestUser { get; set; } = null!;
+
+        [Required]
+        public int SearchUserId { get; set; }
+
+        [ForeignKey(nameof(SearchUserId))]
+        public SearchUser SearchUser { get; set; } = null!;
+
+        public IEnumerable<Answer> Answers = new List<Answer>();
     }
 }
