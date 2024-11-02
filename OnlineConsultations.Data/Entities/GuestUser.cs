c1c50f5398ecbf23934b9716d4a5853c46730259
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-02 17:56:35 +0200

    Added properties

--- OnlineConsultations.Data/Entities/GuestUser.cs
+++ OnlineConsultations.Data/Entities/GuestUser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,18 @@ namespace OnlineConsultations.Data.Entities
 {
     internal class GuestUser : ApplicationUser
     {
+
+        [Required]
+        public int GuestUserId { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; } = null!;
+
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser? ApplicationUser { get; set; }
+
+        public IEnumerable<Question> Questions = new List<Question>();
+
+        public IEnumerable<Comment> Comments = new List<Comment>();
     }
 }
