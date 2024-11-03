b780ecc6e33e034269a85c3237072ad566995d8c
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-03 17:04:18 +0200

    Added data

--- OnlineConsultations.Data/Seeds/SearchUserConfiguration.cs
+++ OnlineConsultations.Data/Seeds/SearchUserConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OnlineConsultations.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace OnlineConsultations.Data.Seeds
 {
-    internal class SearchUserConfiguration
+    internal class SearchUserConfiguration : IEntityTypeConfiguration<SearchUser>
     {
+        public void Configure(EntityTypeBuilder<SearchUser> builder)
+        {
+            builder.HasData(CreateSearchUsers());
+        }
+
+        private static List<SearchUser> CreateSearchUsers()
+        {
+            var searchUsers = new List<SearchUser>()
+            {
+                new SearchUser()
+                {
+                   SearchUserId = 1,
+                   ApplicationUserId = "search1@search.com"
+                },
+                new SearchUser()
+                {
+                   SearchUserId = 2,
+                   ApplicationUserId = "search2@search.com"
+                },
+                new SearchUser()
+                {
+                   SearchUserId = 3,
+                   ApplicationUserId = "search3@search.com"
+                },
+
+            };
+
+            return searchUsers;
+        }
     }
 }
