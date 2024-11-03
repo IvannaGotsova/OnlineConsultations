372e54fa9933803353a04f7a9676acbbb453e7fe
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-03 12:22:30 +0200

    Modify Program.cs

--- OnlineConsultations/Program.cs
+++ OnlineConsultations/Program.cs
@@ -1,41 +1,94 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineConsultations.Data;
+using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<OnlineConsultationsDbContext>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<OnlineConsultationsDbContext>
+    (options => {
+        options.UseSqlServer(connectionString, b => b.MigrationsAssembly("OnlineConsultations.Data"));
+        options.UseSqlServer(connectionString);
+    });
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<OnlineConsultationsDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddDefaultIdentity<ApplicationUser>
+    (
+    options => {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireDigit = true;
+        options.User.RequireUniqueEmail = true;
+
+    })
+    .AddRoles<ApplicationRole>()
+    .AddEntityFrameworkStores <OnlineConsultationsDbContext>();
+
+
+builder.Services.AddControllersWithViews(
+    options => {
+        options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
+    });
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/ApplicationUsers/Login";
+});
+
+builder.Services.AddScoped<IRepository, Repository>();
+// !!! to add services later
+
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
+
+// app.SeedUsersRoles();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    app.UseExceptionHandler("/Home/Error");
 }
 else
 {
+    app.UseStatusCodePages();
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseHsts();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapDefaultControllerRoute();
+app.UseEndpoints(endpoints =>
+{
+
+
+    app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
+    );
+
+    app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+
+});
+
 app.MapRazorPages();
 
 app.Run();
