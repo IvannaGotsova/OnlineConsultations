09c2c81330148cbfc8553334ce8979dc8532b4c2
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-03 17:03:54 +0200

    Added data

--- OnlineConsultations.Data/Seeds/PostConfiguration.cs
+++ OnlineConsultations.Data/Seeds/PostConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OnlineConsultations.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,45 @@ using System.Threading.Tasks;
 
 namespace OnlineConsultations.Data.Seeds
 {
-    internal class PostConfiguration
+    internal class PostConfiguration : IEntityTypeConfiguration<Post>
     {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.HasData(CreatePosts());
+        }
+
+        private static List<Post> CreatePosts()
+        {
+            var posts = new List<Post>()
+            {
+                new Post()
+                {
+                    PostId = 1, 
+                    PostTitle = "First post", 
+                    PostDescription = "First Description", 
+                    PostPayPerHour = 22,
+                    ProvideUserId = 1
+                },
+                new Post()
+                {
+                    PostId = 2,
+                    PostTitle = "Second post",
+                    PostDescription = "Second Description",
+                    PostPayPerHour = 24,
+                    ProvideUserId = 2
+                },
+                new Post()
+                {
+                    PostId = 3,
+                    PostTitle = "Third post",
+                    PostDescription = "Third Description",
+                    PostPayPerHour = 22,
+                    ProvideUserId = 3
+                },
+
+            };
+
+            return posts;
+        }
     }
 }
