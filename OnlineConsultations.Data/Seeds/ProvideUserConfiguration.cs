f8b93f34522313c782c323d0c07d1c935b801635
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-03 17:03:58 +0200

    Added data

--- OnlineConsultations.Data/Seeds/ProvideUserConfiguration.cs
+++ OnlineConsultations.Data/Seeds/ProvideUserConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OnlineConsultations.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace OnlineConsultations.Data.Seeds
 {
-    internal class ProvideUserConfiguration
+    internal class ProvideUserConfiguration : IEntityTypeConfiguration<ProvideUser>
     {
+        public void Configure(EntityTypeBuilder<ProvideUser> builder)
+        {
+            builder.HasData(CreateProvideUsers());
+        }
+
+        private static List<ProvideUser> CreateProvideUsers()
+        {
+            var provideUsers = new List<ProvideUser>()
+            {
+                new ProvideUser()
+                {
+                   ProvideUserId = 1,
+                   ApplicationUserId = "provide1@provide.com", 
+                },                               
+                new ProvideUser()                
+                {                                
+                   ProvideUserId = 2,            
+                   ApplicationUserId = "provide2@provide.com"
+                },                               
+                new ProvideUser()                
+                {                                
+                   ProvideUserId = 3,            
+                   ApplicationUserId = "provide3@provide.com"
+                },
+
+            };
+
+            return provideUsers;
+        }
     }
 }
