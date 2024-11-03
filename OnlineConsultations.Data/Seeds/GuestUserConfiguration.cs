0dd54b6a6619f5c6273aecc4d1b0da64a1b15042
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-03 17:03:46 +0200

    Added data

--- OnlineConsultations.Data/Seeds/GuestUserConfiguration.cs
+++ OnlineConsultations.Data/Seeds/GuestUserConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineConsultations.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace OnlineConsultations.Data.Seeds
 {
-    internal class GuestUserConfiguration
+    internal class GuestUserConfiguration : IEntityTypeConfiguration<GuestUser>
     {
+        public void Configure(EntityTypeBuilder<GuestUser> builder)
+        {
+            builder.HasData(CreateGuestUsers());
+        }
+
+        private static List<GuestUser> CreateGuestUsers()
+        {
+            var guestUsers = new List<GuestUser>()
+            {
+                new GuestUser()
+                {
+                   GuestUserId = 1,
+                   ApplicationUserId = "guest1@guest.com"
+                },
+                new GuestUser()
+                {
+                   GuestUserId = 2,
+                   ApplicationUserId = "guest2@guest.com"
+                },
+                new GuestUser()
+                {
+                   GuestUserId = 3,
+                   ApplicationUserId = "guest3@guest.com"
+                },
+
+            };
+
+            return guestUsers;
+        }
     }
 }
