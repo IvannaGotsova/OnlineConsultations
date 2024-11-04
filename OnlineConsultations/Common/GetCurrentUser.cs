d9082244716fe56b5df1913a8ee277f960041758
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-04 22:31:53 +0200

    Created GetCurrentUser

---
+++ OnlineConsultations/Common/GetCurrentUser.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace OnlineConsultations.Common
+{
+    public static class GetCurrentUser
+    {
+       
+        public static string GetCurrentUserId(this ClaimsPrincipal claimsPrincipalUser)
+        {
+            return claimsPrincipalUser.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+        
+        public static string GetCurrentUserName(this ClaimsPrincipal claimsPrincipalUser)
+        {
+            return claimsPrincipalUser.FindFirstValue(ClaimTypes.Name);
+        }
+    }
+}
