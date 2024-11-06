67f4a55f47d99ee3da1032fbf1056ab42d168fb2
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-06 21:19:44 +0200

    Modify properties

--- OnlineConsultations.Data/Entities/Post.cs
+++ OnlineConsultations.Data/Entities/Post.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static OnlineConsultations.Data.DataConstants.DataConstants.PostConstants;
+using Microsoft.EntityFrameworkCore;
 
 namespace OnlineConsultations.Data.Entities
 {
@@ -24,6 +25,7 @@ namespace OnlineConsultations.Data.Entities
         public required string PostDescription { get; set; }
 
         [Required]
+        [Range(typeof(double), "0.00", "10000.00", ConvertValueInInvariantCulture = true)]
         public  decimal PostPayPerHour { get; set; }
 
         [Required]
