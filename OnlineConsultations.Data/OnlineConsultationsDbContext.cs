87f4b4d5b4b49ef75ce1c9ced11ee0e41210b568
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-06 21:57:08 +0200

    Modify context

--- OnlineConsultations.Data/OnlineConsultationsDbContext.cs
+++ OnlineConsultations.Data/OnlineConsultationsDbContext.cs
@@ -25,14 +25,14 @@ namespace OnlineConsultations.Data
         public DbSet<ApplicationRole> ApplicationRoles { get; set; } = null!;
         public DbSet<Answer> Answers { get; set; } = null!;
         public DbSet<ApplicationUser> ApplicationUsers { get; set; } = null!;
-        public DbSet<Comment> Comments { get; set; } = null!;
-        public DbSet<GuestUser> GuestUsers { get; set; } = null!;
+        public DbSet<Comment> Comments { get; set; } = null!; 
         public DbSet<Message> Messages { get; set; } = null!;
         public DbSet<Post> Posts { get; set; } = null!;
         public DbSet<ProvideUser> ProvideUsers { get; set; } = null!;
         public DbSet<Question> Questions { get; set; } = null!;
         public DbSet<Rating> Ratings { get; set; } = null!;
         public DbSet<Review> Reviews { get; set; } = null!;
+        public DbSet<GuestUser> GuestUsers { get; set; } = null!;
         public DbSet<SearchUser> SearchUsers { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder builder)
