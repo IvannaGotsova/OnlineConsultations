f43768bd17d74f10728c65caa9e07b005e417a9f
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-09 21:08:21 +0200

    Modify model

--- OnlineConsultations.Data/Models/ApplicationUserModels/LoginModelView.cs
+++ OnlineConsultations.Data/Models/ApplicationUserModels/LoginModelView.cs
@@ -1,6 +1,16 @@
-﻿namespace OnlineConsultations.Models.ApplicationUserModels
+﻿using System.ComponentModel.DataAnnotations;
+using static OnlineConsultations.Data.DataConstants.DataConstants.ApplicationUserConstants;
+
+namespace OnlineConsultations.Models.ApplicationUserModels
 {
     public class LoginModelView
     {
+        [Required]
+        [StringLength(UserMaxLengthUsername, MinimumLength = UserMinLengthUsername)]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
     }
 }
