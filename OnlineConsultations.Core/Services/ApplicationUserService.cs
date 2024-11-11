78bd4087ec1b12309042b361b06e769f3c04eee7
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-11 21:35:19 +0200

    Added Service with interface

---
+++ OnlineConsultations.Core/Services/ApplicationUserService.cs
@@ -0,0 +1,13 @@
+﻿using OnlineConsultations.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineConsultations.Core.Services
+{
+    public class ApplicationUserService :  IApplicationUserService
+    {
+    }
+}
