5c11a6e15d7bc1c83c1976c4466de17ec21889b7
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-12 21:43:44 +0200

    Added model

---
+++ OnlineConsultations.Data/Models/ApplicationUserModels/ApplicationUserModelView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using static OnlineConsultations.Data.DataConstants.DataConstants.ApplicationUserConstants;
+
+namespace OnlineConsultations.Data.Models.ApplicationUserModels
+{
+    public class ApplicationUserModelView
+    {
+        public string Id { get; set; } = null!;
+        [Required]
+        [StringLength(UserMaxLengthUsername, MinimumLength = UserMinLengthUsername)]
+        public string UserName { get; set; } = null!;
+        [Required]
+        [StringLength(UserMaxLengthFirstName, MinimumLength = UserMinLengthFirstName)]
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(UserMaxLengthLastName, MinimumLength = UserMinLengthLastName)]
+        public string? LastName { get; set; }
+    }
+}
