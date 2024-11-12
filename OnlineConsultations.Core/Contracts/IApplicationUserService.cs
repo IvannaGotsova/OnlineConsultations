703425778a3b3e63c51f68f0914b1c5b876562dc
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-12 21:44:01 +0200

    Added basic Interface methods

--- OnlineConsultations.Core/Contracts/IApplicationUserService.cs
+++ OnlineConsultations.Core/Contracts/IApplicationUserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.ApplicationUserModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,20 @@ namespace OnlineConsultations.Core.Contracts
 {
     public interface IApplicationUserService
     {
+        Task<IEnumerable<ApplicationUserModelView>> GetApplicationUsers();
+
+        Task<IEnumerable<ApplicationUserModelView>> GetApplicationVIPUsers();
+
+        Task<ApplicationUser> GetApplicaionUserById(string userId);
+      
+        Task<ApplicationUserModelView> DeleteCreateForm(string userId);
+     
+        Task Delete(string userId);
+      
+        Task MakeVIP(string userId);
+        
+        Task RemoveVIP(string userId);
+       
+        Task<IEnumerable<ApplicationUser>> GetApplicationUsersForSelect();
     }
 }
