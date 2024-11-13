f57e1f0629a03f6b27d0f329e27b0a7f58eac714
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-13 20:18:29 +0200

    Implement interface

--- OnlineConsultations.Core/Services/ApplicationUserService.cs
+++ OnlineConsultations.Core/Services/ApplicationUserService.cs
@@ -1,4 +1,6 @@
 ﻿using OnlineConsultations.Core.Contracts;
+using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.ApplicationUserModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,46 @@ using System.Threading.Tasks;
 
 namespace OnlineConsultations.Core.Services
 {
-    public class ApplicationUserService :  IApplicationUserService
+    public class ApplicationUserService : IApplicationUserService
     {
+        public Task Delete(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUserModelView> DeleteCreateForm(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUser> GetApplicaionUserById(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ApplicationUserModelView>> GetApplicationUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ApplicationUser>> GetApplicationUsersForSelect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ApplicationUserModelView>> GetApplicationVIPUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task MakeVIP(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveVIP(string userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
