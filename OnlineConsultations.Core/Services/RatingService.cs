cc0a6806892859a9697b1ee51b150fb5c6e0967c
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:09:37 +0200

    Implement interface

--- OnlineConsultations.Core/Services/RatingService.cs
+++ OnlineConsultations.Core/Services/RatingService.cs
@@ -1,4 +1,6 @@
 ﻿using OnlineConsultations.Core.Contracts;
+using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Rating;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,49 @@ namespace OnlineConsultations.Core.Services
 {
     public class RatingService : IRatingService
     {
+        public Task Add(AddRatingModelView addRatingModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int ratingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteRatingModelView> DeleteRatingForm(int ratingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Edit(int ratingId, EditRatingModelView editRatingModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EditRatingModelView> EditCreateForm(int ratingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AllRatingsModelView>> GetAllRatings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Rating> GetRatingById(int ratingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DetailsRatingModelView> GetRatingDetailsById(int ratingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Rating>> GetRatingsForSelect()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
