e2b93d56bf610264d1f49a8663685c507c5ae671
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 20:48:40 +0200

    Modfied

--- OnlineConsultations.Core/Contracts/IAnswerService.cs
+++ OnlineConsultations.Core/Contracts/IAnswerService.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OnlineConsultations.Data.Models.Answer;
 
 namespace OnlineConsultations.Core.Contracts
 {
     public interface IAnswerService
     {
+        Task<IEnumerable<AllCommentsModelView>> GetAllAnswers();
+
+        Task Add(AddCommentModelView addAnswerModel);
+
+        Task<IEnumerable<Answer>> GetAnswersForSelect();
+
+        Task<DetailsCommentModelView> GetAnswerDetailsById(int answerId);
+
+        Task<EditCommentModelView> EditCreateForm(int answerId);
+
+        Task Edit(int answerId, EditCommentModelView editAnswerModel);
+
+        Task<Answer> GetAnswerById(int answerId);
+
+        Task<DeleteCommentModelView> DeleteAnswerForm(int answerId);
+
+        Task Delete(int answerId);
     }
 }
