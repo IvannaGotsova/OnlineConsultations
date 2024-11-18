15e4dbff8a7a48aaaed3c440b9b3e84aa581da89
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:05:08 +0200

    Added basic methods

--- OnlineConsultations.Core/Contracts/IGuestUserService.cs
+++ OnlineConsultations.Core/Contracts/IGuestUserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.GuestUser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace OnlineConsultations.Core.Contracts
 {
     public interface IGuestUserService
     {
+        Task<IEnumerable<AllGuestUsersModelView>> GetAllGuestUsers();
+
+        Task Add(AddGuestUserModelView addGuestUserModel);
+
+        Task<IEnumerable<GuestUser>> GetGuestUsersForSelect();
+
+        Task<DetailsGuestUserModelView> GetGuestUserDetailsById(int guestUserId);
+
+        Task<EditGuestUserModelView> EditCreateForm(int guestUserId);
+
+        Task Edit(int guestUserId, EditGuestUserModelView editGuestUserModel);
+
+        Task<GuestUser> GetGuestUserById(int guestUserId);
+
+        Task<DeleteGuestUserModelView> DeleteGuestUserForm(int guestUserId);
+
+        Task Delete(int guestUserId);
     }
 }
