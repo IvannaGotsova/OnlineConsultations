dbc731469c55b36bda006976f665ee1d3ea0bee9
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:05:35 +0200

    Added basic methods

--- OnlineConsultations.Core/Contracts/IQuestionService.cs
+++ OnlineConsultations.Core/Contracts/IQuestionService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Question;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace OnlineConsultations.Core.Contracts
 {
     public interface IQuestionService
     {
+        Task<IEnumerable<AllQuestionsModelView>> GetAllQuestions();
+
+        Task Add(AddQuestionModelView addQuestionModel);
+
+        Task<IEnumerable<Question>> GetQuestionsForSelect();
+
+        Task<DetailsQuestionModelView> GetQuestionDetailsById(int questionId);
+
+        Task<EditQuestionModelView> EditCreateForm(int questionId);
+
+        Task Edit(int questionId, EditQuestionModelView editQuestionModel);
+
+        Task<Question> GetQuestionById(int questionId);
+
+        Task<DeleteQuestionModelView> DeleteQuestionForm(int questionId);
+
+        Task Delete(int questionId);
     }
 }
