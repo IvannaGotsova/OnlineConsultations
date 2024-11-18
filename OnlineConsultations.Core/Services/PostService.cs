3306df9d3de331c5fd2e18bb4438bcc89bfeb8e5
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:09:23 +0200

    Implement interface

--- OnlineConsultations.Core/Services/PostService.cs
+++ OnlineConsultations.Core/Services/PostService.cs
@@ -1,4 +1,6 @@
 ﻿using OnlineConsultations.Core.Contracts;
+using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Post;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,49 @@ namespace OnlineConsultations.Core.Services
 {
     public class PostService : IPostService
     {
+        public Task Add(AddPostModelView addPostModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeletePostModelView> DeletePostForm(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Edit(int postId, EditPostModelView editPostModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EditPostModelView> EditCreateForm(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AllPostsModelView>> GetAllPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPostById(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DetailsPostModelView> GetPostDetailsById(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Post>> GetPostsForSelect()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
