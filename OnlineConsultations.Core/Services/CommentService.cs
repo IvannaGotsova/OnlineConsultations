7eec506d5f5fc33d7447b1c17c0d5beadac362c0
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:09:10 +0200

    Implement interface

--- OnlineConsultations.Core/Services/CommentService.cs
+++ OnlineConsultations.Core/Services/CommentService.cs
@@ -1,4 +1,6 @@
 ﻿using OnlineConsultations.Core.Contracts;
+using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Comment;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,49 @@ namespace OnlineConsultations.Core.Services
 {
     public class CommentService : ICommentService
     {
+        public Task Add(AddCommentModelView addCommentModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteCommentModelView> DeleteCommentForm(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Edit(int commentId, EditCommentModelView editCommentModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EditCommentModelView> EditCreateForm(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AllCommentsModelView>> GetAllComments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Comment> GetCommentById(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DetailsCommentModelView> GetCommentDetailsById(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Comment>> GetCommentsForSelect()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
