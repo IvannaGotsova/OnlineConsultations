9fd56bda52253a4158df5938d0aa29343ac88cd5
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 20:55:31 +0200

    Corrected errors

--- OnlineConsultations.Core/Contracts/IAnswerService.cs
+++ OnlineConsultations.Core/Contracts/IAnswerService.cs
@@ -1,30 +1,30 @@
 ﻿using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Answer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OnlineConsultations.Data.Models.Answer;
 
 namespace OnlineConsultations.Core.Contracts
 {
     public interface IAnswerService
     {
-        Task<IEnumerable<AllCommentsModelView>> GetAllAnswers();
+        Task<IEnumerable<AllAnswersModelView>> GetAllAnswers();
 
-        Task Add(AddCommentModelView addAnswerModel);
+        Task Add(AddAnswerModelView addAnswerModel);
 
         Task<IEnumerable<Answer>> GetAnswersForSelect();
 
-        Task<DetailsCommentModelView> GetAnswerDetailsById(int answerId);
+        Task<DetailsAnswerModelView> GetAnswerDetailsById(int answerId);
 
-        Task<EditCommentModelView> EditCreateForm(int answerId);
+        Task<EditAnswerModelView> EditCreateForm(int answerId);
 
-        Task Edit(int answerId, EditCommentModelView editAnswerModel);
+        Task Edit(int answerId, EditAnswerModelView editAnswerModel);
 
         Task<Answer> GetAnswerById(int answerId);
 
-        Task<DeleteCommentModelView> DeleteAnswerForm(int answerId);
+        Task<DeleteAnswerModelView> DeleteAnswerForm(int answerId);
 
         Task Delete(int answerId);
     }
