87d8aa1b9123c9c9a7bee607a9a793be8131c855
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:09:40 +0200

    Implement interface

--- OnlineConsultations.Core/Services/ReviewService.cs
+++ OnlineConsultations.Core/Services/ReviewService.cs
@@ -1,4 +1,6 @@
 ﻿using OnlineConsultations.Core.Contracts;
+using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Review;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,49 @@ namespace OnlineConsultations.Core.Services
 {
     public class ReviewService : IReviewService
     {
+        public Task Add(AddReviewModelView addReviewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int reviewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteReviewModelView> DeleteReviewForm(int reviewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Edit(int reviewId, EditReviewModelView editReviewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EditReviewModelView> EditCreateForm(int reviewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AllReviewsModelView>> GetAllReviews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Review> GetReviewById(int reviewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DetailsReviewModelView> GetReviewDetailsById(int reviewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Review>> GetReviewsForSelect()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
