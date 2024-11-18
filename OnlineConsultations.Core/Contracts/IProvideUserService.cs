a60262925c2889f449794b07dd749c0266d131ab
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:05:27 +0200

    Added basic methods

--- OnlineConsultations.Core/Contracts/IProvideUserService.cs
+++ OnlineConsultations.Core/Contracts/IProvideUserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.ProvideUser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace OnlineConsultations.Core.Contracts
 {
     public interface IProvideUserService
     {
+        Task<IEnumerable<AllProvideUsersModelView>> GetAllProvideUsers();
+
+        Task Add(AddProvideUserModelView addProvideUserModel);
+
+        Task<IEnumerable<ProvideUser>> GetProvideUsersForSelect();
+
+        Task<DetailsProvideUserModelView> GetProvideUserDetailsById(int provideUserId);
+
+        Task<EditProvideUserModelView> EditCreateForm(int provideUserId);
+
+        Task Edit(int provideUserId, EditProvideUserModelView editProvideUserModel);
+
+        Task<ProvideUser> GetProvideUserById(int provideUserId);
+
+        Task<DeleteProvideUserModelView> DeleteProvideUserForm(int provideUserId);
+
+        Task Delete(int provideUserId);
     }
 }
