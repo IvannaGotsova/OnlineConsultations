9da060b2286f414dd3f7460d9b60656669689881
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:05:40 +0200

    Added basic methods

--- OnlineConsultations.Core/Contracts/IRatingService.cs
+++ OnlineConsultations.Core/Contracts/IRatingService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Rating;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace OnlineConsultations.Core.Contracts
 {
     public interface IRatingService
     {
+        Task<IEnumerable<AllRatingsModelView>> GetAllRatings();
+
+        Task Add(AddRatingModelView addRatingModel);
+
+        Task<IEnumerable<Rating>> GetRatingsForSelect();
+
+        Task<DetailsRatingModelView> GetRatingDetailsById(int ratingId);
+
+        Task<EditRatingModelView> EditCreateForm(int ratingId);
+
+        Task Edit(int ratingId, EditRatingModelView editRatingModel);
+
+        Task<Rating> GetRatingById(int ratingId);
+
+        Task<DeleteRatingModelView> DeleteRatingForm(int ratingId);
+
+        Task Delete(int ratingId);
     }
 }
