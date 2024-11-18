60747c82f7cdd5435422c40b3f92ba5d16c20b00
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:05:45 +0200

    Added basic methods

--- OnlineConsultations.Core/Contracts/IReviewService.cs
+++ OnlineConsultations.Core/Contracts/IReviewService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Review;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace OnlineConsultations.Core.Contracts
 {
     public interface IReviewService
     {
+        Task<IEnumerable<AllReviewsModelView>> GetAllReviews();
+
+        Task Add(AddReviewModelView addReviewModel);
+
+        Task<IEnumerable<Review>> GetReviewsForSelect();
+
+        Task<DetailsReviewModelView> GetReviewDetailsById(int reviewId);
+
+        Task<EditReviewModelView> EditCreateForm(int reviewId);
+
+        Task Edit(int reviewId, EditReviewModelView editReviewModel);
+
+        Task<Review> GetReviewById(int reviewId);
+
+        Task<DeleteReviewModelView> DeleteReviewForm(int reviewId);
+
+        Task Delete(int reviewId);
     }
 }
