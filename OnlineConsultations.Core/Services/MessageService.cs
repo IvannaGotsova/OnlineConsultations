4aacda33c1e3bb4f76a3e65c176d2d08f8af1fd6
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:09:18 +0200

    Implement interface

--- OnlineConsultations.Core/Services/MessageService.cs
+++ OnlineConsultations.Core/Services/MessageService.cs
@@ -1,4 +1,6 @@
 ﻿using OnlineConsultations.Core.Contracts;
+using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Message;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,49 @@ namespace OnlineConsultations.Core.Services
 {
     public class MessageService : IMessageService
     {
+        public Task Add(AddMessageModelView addMessageModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int messageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteMessageModelView> DeleteMessageForm(int messageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Edit(int messageId, EditMessageModelView editMessageModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EditMessageModelView> EditCreateForm(int messageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AllMessagesModelView>> GetAllMessages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Message> GetMessageById(int messageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DetailsMessageModelView> GetMessageDetailsById(int messageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Message>> GetMessagesForSelect()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
