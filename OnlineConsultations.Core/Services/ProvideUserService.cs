6f94aacf0d467c3a49ce58040d0b8f5e8bcf0bdd
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:09:29 +0200

    Implement interface

--- OnlineConsultations.Core/Services/ProvideUserService.cs
+++ OnlineConsultations.Core/Services/ProvideUserService.cs
@@ -1,4 +1,6 @@
 ﻿using OnlineConsultations.Core.Contracts;
+using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.ProvideUser;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,49 @@ namespace OnlineConsultations.Core.Services
 {
     public class ProvideUserService : IProvideUserService
     {
+        public Task Add(AddProvideUserModelView addProvideUserModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int provideUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteProvideUserModelView> DeleteProvideUserForm(int provideUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Edit(int provideUserId, EditProvideUserModelView editProvideUserModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EditProvideUserModelView> EditCreateForm(int provideUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AllProvideUsersModelView>> GetAllProvideUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProvideUser> GetProvideUserById(int provideUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DetailsProvideUserModelView> GetProvideUserDetailsById(int provideUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProvideUser>> GetProvideUsersForSelect()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
