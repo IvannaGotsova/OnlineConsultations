0ce5351427e9dde6419e9df8b637bca8e6b29814
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:05:05 +0200

    Added basic methods

--- OnlineConsultations.Core/Contracts/ICommentService.cs
+++ OnlineConsultations.Core/Contracts/ICommentService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Comment;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace OnlineConsultations.Core.Contracts
 {
     public interface ICommentService
     {
+        Task<IEnumerable<AllCommentsModelView>> GetAllComments();
+
+        Task Add(AddCommentModelView addCommentModel);
+
+        Task<IEnumerable<Comment>> GetCommentsForSelect();
+
+        Task<DetailsCommentModelView> GetCommentDetailsById(int commentId);
+
+        Task<EditCommentModelView> EditCreateForm(int commentId);
+
+        Task Edit(int commentId, EditCommentModelView editCommentModel);
+
+        Task<Comment> GetCommentById(int commentId);
+
+        Task<DeleteCommentModelView> DeleteCommentForm(int commentId);
+
+        Task Delete(int commentId);
     }
 }
