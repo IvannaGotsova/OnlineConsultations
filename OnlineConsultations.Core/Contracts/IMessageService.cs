7aa99ebed18f9888c7d9ac5fed2a2fdd53f620fb
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:05:13 +0200

    Added basic methods

--- OnlineConsultations.Core/Contracts/IMessageService.cs
+++ OnlineConsultations.Core/Contracts/IMessageService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Message;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace OnlineConsultations.Core.Contracts
 {
     public interface IMessageService
     {
+        Task<IEnumerable<AllMessagesModelView>> GetAllMessages();
+
+        Task Add(AddMessageModelView addMessageModel);
+
+        Task<IEnumerable<Message>> GetMessagesForSelect();
+
+        Task<DetailsMessageModelView> GetMessageDetailsById(int messageId);
+
+        Task<EditMessageModelView> EditCreateForm(int messageId);
+
+        Task Edit(int messageId, EditMessageModelView editMessageModel);
+
+        Task<Message> GetMessageById(int messageId);
+
+        Task<DeleteMessageModelView> DeleteMessageForm(int messageId);
+
+        Task Delete(int messageId);
     }
 }
