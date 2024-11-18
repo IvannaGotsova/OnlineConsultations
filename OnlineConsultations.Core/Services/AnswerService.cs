6c6c363f8cf2e63ea1bf02bcb7c486cab220cd39
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 20:48:47 +0200

    Modified

--- OnlineConsultations.Core/Services/AnswerService.cs
+++ OnlineConsultations.Core/Services/AnswerService.cs
@@ -1,4 +1,6 @@
 ﻿using OnlineConsultations.Core.Contracts;
+using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Answer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,49 @@ namespace OnlineConsultations.Core.Services
 {
     public class AnswerService : IAnswerService
     {
+        public Task Add(AddCommentModelView addAnswerModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int answerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteCommentModelView> DeleteAnswerForm(int answerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Edit(int answerId, EditCommentModelView editAnswerModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EditCommentModelView> EditCreateForm(int answerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AllCommentsModelView>> GetAllAnswers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Answer> GetAnswerById(int answerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DetailsCommentModelView> GetAnswerDetailsById(int answerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Answer>> GetAnswersForSelect()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
