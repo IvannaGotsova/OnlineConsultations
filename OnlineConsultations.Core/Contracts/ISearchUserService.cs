09b7ee6241dc93c128c3bc1a854a9bce7bd8414e
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:05:51 +0200

    Added basic methods

--- OnlineConsultations.Core/Contracts/ISearchUserService.cs
+++ OnlineConsultations.Core/Contracts/ISearchUserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.SearchUser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace OnlineConsultations.Core.Contracts
 {
     public interface ISearchUserService
     {
+        Task<IEnumerable<AllSearchUsersModelView>> GetAllSearchUsers();
+
+        Task Add(AddSearchUserModelView addSearchUserModel);
+
+        Task<IEnumerable<SearchUser>> GetSearchUsersForSelect();
+
+        Task<DetailsSearchUserModelView> GetSearchUserDetailsById(int searchUserId);
+
+        Task<EditSearchUserModelView> EditCreateForm(int searchUserId);
+
+        Task Edit(int searchUserId, EditSearchUserModelView editSearchUserModel);
+
+        Task<SearchUser> GetSearchUserById(int searchUserId);
+
+        Task<DeleteSearchUserModelView> DeleteSearchUserForm(int searchUserId);
+
+        Task Delete(int searchUserId);
     }
 }
