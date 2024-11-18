9293fb479fd4702565e10d4a22b795c64bdf6199
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:09:32 +0200

    Implement interface

--- OnlineConsultations.Core/Services/QuestionService.cs
+++ OnlineConsultations.Core/Services/QuestionService.cs
@@ -1,4 +1,6 @@
 ﻿using OnlineConsultations.Core.Contracts;
+using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Question;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,49 @@ namespace OnlineConsultations.Core.Services
 {
     public class QuestionService : IQuestionService
     {
+        public Task Add(AddQuestionModelView addQuestionModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteQuestionModelView> DeleteQuestionForm(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Edit(int questionId, EditQuestionModelView editQuestionModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EditQuestionModelView> EditCreateForm(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AllQuestionsModelView>> GetAllQuestions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Question> GetQuestionById(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DetailsQuestionModelView> GetQuestionDetailsById(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Question>> GetQuestionsForSelect()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
