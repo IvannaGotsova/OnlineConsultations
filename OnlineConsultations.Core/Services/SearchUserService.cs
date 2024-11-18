1da27e88939e3fc7b40e69079c50f0120baf92cb
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:09:42 +0200

    Implement interface

--- OnlineConsultations.Core/Services/SearchUserService.cs
+++ OnlineConsultations.Core/Services/SearchUserService.cs
@@ -1,4 +1,6 @@
 ﻿using OnlineConsultations.Core.Contracts;
+using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.SearchUser;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,49 @@ namespace OnlineConsultations.Core.Services
 {
     public class SearchUserService : ISearchUserService
     {
+        public Task Add(AddSearchUserModelView addSearchUserModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int searchUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteSearchUserModelView> DeleteSearchUserForm(int searchUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Edit(int searchUserId, EditSearchUserModelView editSearchUserModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EditSearchUserModelView> EditCreateForm(int searchUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AllSearchUsersModelView>> GetAllSearchUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SearchUser> GetSearchUserById(int searchUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DetailsSearchUserModelView> GetSearchUserDetailsById(int searchUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SearchUser>> GetSearchUsersForSelect()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
