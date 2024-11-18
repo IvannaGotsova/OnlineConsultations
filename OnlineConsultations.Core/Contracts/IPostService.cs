0725103ac14b5bceabd0294b2aaecaa06d6c6012
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-18 21:05:20 +0200

    Added basic methods

--- OnlineConsultations.Core/Contracts/IPostService.cs
+++ OnlineConsultations.Core/Contracts/IPostService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Post;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace OnlineConsultations.Core.Contracts
 {
     public interface IPostService
     {
+        Task<IEnumerable<AllPostsModelView>> GetAllPosts();
+
+        Task Add(AddPostModelView addPostModel);
+
+        Task<IEnumerable<Post>> GetPostsForSelect();
+
+        Task<DetailsPostModelView> GetPostDetailsById(int postId);
+
+        Task<EditPostModelView> EditCreateForm(int postId);
+
+        Task Edit(int postId, EditPostModelView editPostModel);
+
+        Task<Post> GetPostById(int postId);
+
+        Task<DeletePostModelView> DeletePostForm(int postId);
+
+        Task Delete(int postId);
     }
 }
