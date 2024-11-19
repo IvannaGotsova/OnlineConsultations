a101519855d004c0d12379ac4aa0f84abdf08ffd
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-19 21:47:39 +0200

    Adde basic properties

--- OnlineConsultations.Data/Models/ProvideUser/AllProvideUsersModelView.cs
+++ OnlineConsultations.Data/Models/ProvideUser/AllProvideUsersModelView.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,20 @@ namespace OnlineConsultations.Data.Models.ProvideUser
 {
     public class AllProvideUsersModelView
     {
+        [Key]
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [Required]
+        public required string ApplicationUserId { get; set; }
+
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser? ApplicationUser { get; set; }
+
+        public IEnumerable<Entities.Post> Posts = new List<Entities.Post>();
+
+        public IEnumerable<Entities.Answer> Answers = new List<Entities.Answer>();
+
+        public IEnumerable<Entities.Message> Messages = new List<Entities.Message>();
     }
 }
