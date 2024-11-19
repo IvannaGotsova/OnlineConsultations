bc250dbb94b0ecd69a7ac45fd800d652f6670a86
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-19 21:47:43 +0200

    Adde basic properties

--- OnlineConsultations.Data/Models/Question/DeleteQuestionModelView.cs
+++ OnlineConsultations.Data/Models/Question/DeleteQuestionModelView.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.QuestionConstants;
+
 
 namespace OnlineConsultations.Data.Models.Question
 {
     public class DeleteQuestionModelView
     {
+        [Key]
+        [Required]
+        public int QuestionId { get; set; }
+
+        [Required]
+        [StringLength(QuestionTitleMaxLength, MinimumLength = QuestionTitleMinLength)]
+        public required string QuestionTitle { get; set; }
+
+        [Required]
+        [StringLength(QuestionDescriptionMaxLength, MinimumLength = QuestionDescriptionMinLength)]
+        public required string QuestionDescription { get; set; }
+
+        [Required]
+        public DateTime QuestionDate { get; set; }
+
+        public int? GuestUserId { get; set; } = null;
+
+        [ForeignKey(nameof(GuestUserId))]
+        public Entities.GuestUser? GuestUser { get; set; }
+
+        public int? SearchUserId { get; set; } = null;
+
+        [ForeignKey(nameof(SearchUserId))]
+        public Entities.SearchUser? SearchUser { get; set; }
+
+        public IEnumerable<Entities.Answer> Answers = new List<Entities.Answer>();
     }
 }
--- OnlineConsultations.Data/Models/Question/EditQuestionModelView.cs
+++ OnlineConsultations.Data/Models/Question/EditQuestionModelView.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.QuestionConstants;
 
 namespace OnlineConsultations.Data.Models.Question
 {
     public class EditQuestionModelView
     {
+        [Key]
+        [Required]
+        public int QuestionId { get; set; }
+
+        [Required]
+        [StringLength(QuestionTitleMaxLength, MinimumLength = QuestionTitleMinLength)]
+        public required string QuestionTitle { get; set; }
+
+        [Required]
+        [StringLength(QuestionDescriptionMaxLength, MinimumLength = QuestionDescriptionMinLength)]
+        public required string QuestionDescription { get; set; }
+
+        [Required]
+        public DateTime QuestionDate { get; set; }
+
+        public int? GuestUserId { get; set; } = null;
+
+        [ForeignKey(nameof(GuestUserId))]
+        public Entities.GuestUser? GuestUser { get; set; }
+
+        public int? SearchUserId { get; set; } = null;
+
+        [ForeignKey(nameof(SearchUserId))]
+        public Entities.SearchUser? SearchUser { get; set; }
+
+        public IEnumerable<Entities.Answer> Answers = new List<Entities.Answer>();
     }
 }
