2b95983cea3ce1a78779bcccb61fa571aa1ba896
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-19 21:47:08 +0200

    Adde basic properties

--- OnlineConsultations.Data/Models/Answer/DeleteAnswerModelView.cs
+++ OnlineConsultations.Data/Models/Answer/DeleteAnswerModelView.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.AnswerConstants;
 
 namespace OnlineConsultations.Data.Models.Answer
 {
     public class DeleteAnswerModelView
     {
+        [Key]
+        [Required]
+        public int AnswerId { get; set; }
+
+        [Required]
+        [StringLength(AnswerDescriptionMaxLength, MinimumLength = AnswerDescriptionMinLength)]
+        public required string AnswerDescription { get; set; }
+
+        [Required]
+        public DateTime AnswerDate { get; set; }
+
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public Entities.ProvideUser? ProvideUser { get; set; }
+
+        [Required]
+        public int QuestionId { get; set; }
+
+        [ForeignKey(nameof(QuestionId))]
+        public Entities.Question? Question { get; set; }
     }
 }
