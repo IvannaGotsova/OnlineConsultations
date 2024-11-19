c6d1bac8d2c5d0fe34dcbd942030800cdd41b921
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-19 21:47:20 +0200

    Adde basic properties

--- OnlineConsultations.Data/Models/GuestUser/AddGuestUserModelView.cs
+++ OnlineConsultations.Data/Models/GuestUser/AddGuestUserModelView.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,14 @@ namespace OnlineConsultations.Data.Models.GuestUser
 {
     public class AddGuestUserModelView
     {
+        [Required]
+        public required string ApplicationUserId { get; set; }
+
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser? ApplicationUser { get; set; }
+
+        public IEnumerable<Entities.Question> Questions = new List<Entities.Question>();
+
+        public IEnumerable<Entities.Comment> Comments = new List<Entities.Comment>();
     }
 }
--- OnlineConsultations.Data/Models/GuestUser/DeleteGuestUserModelView.cs
+++ OnlineConsultations.Data/Models/GuestUser/DeleteGuestUserModelView.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using OnlineConsultations.Data.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,18 @@ namespace OnlineConsultations.Data.Models.GuestUser
 {
     public class DeleteGuestUserModelView
     {
+        [Key]
+        [Required]
+        public int GuestUserId { get; set; }
+
+        [Required]
+        public required string ApplicationUserId { get; set; }
+
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser? ApplicationUser { get; set; }
+
+        public IEnumerable<Entities.Question> Questions = new List<Entities.Question>();
+
+        public IEnumerable<Entities.Comment> Comments = new List<Entities.Comment>();
     }
 }
