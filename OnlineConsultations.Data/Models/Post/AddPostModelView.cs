b64b7ea3f316e6a1be9c43742df4b78c6525abfd
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-19 21:47:32 +0200

    Adde basic properties

--- OnlineConsultations.Data/Models/Post/AddPostModelView.cs
+++ OnlineConsultations.Data/Models/Post/AddPostModelView.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.PostConstants;
 
 namespace OnlineConsultations.Data.Models.Post
 {
     public class AddPostModelView
     {
+        [Required]
+        [StringLength(PostTitleMaxLength, MinimumLength = PostTitleMinLength)]
+        public required string PostTitle { get; set; }
+
+        [Required]
+        [StringLength(PostDescriptionMaxLength, MinimumLength = PostDescriptionMinLength)]
+        public required string PostDescription { get; set; }
+
+        [Required]
+        [Range(typeof(double), "0.00", "10000.00", ConvertValueInInvariantCulture = true)]
+        public decimal PostPayPerHour { get; set; }
+
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public Entities.ProvideUser? ProvideUser { get; set; }
     }
 }
