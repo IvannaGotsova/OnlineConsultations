d6678fd5ae088c08d5d695ca6b70198382ddd8ff
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-19 21:47:57 +0200

    Adde basic properties

--- OnlineConsultations.Data/Models/Review/DeleteReviewModelView.cs
+++ OnlineConsultations.Data/Models/Review/DeleteReviewModelView.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.ReviewConstants;
 
 namespace OnlineConsultations.Data.Models.Review
 {
     public class DeleteReviewModelView
     {
+        [Key]
+        [Required]
+        public int ReviewId { get; set; }
+
+        [Required]
+        [StringLength(ReviewDescriptionMaxLength, MinimumLength = ReviewDescriptionMinLength)]
+        public required string ReviewDescription { get; set; }
+
+        [Required]
+        public DateTime ReviewDate { get; set; }
+
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public Entities.ProvideUser? ProvideUser { get; set; }
+
+        [Required]
+        public int SearchUserId { get; set; }
+
+        [ForeignKey(nameof(SearchUserId))]
+        public Entities.SearchUser? SearchUser { get; set; }
     }
 }
--- OnlineConsultations.Data/Models/Review/DetailsReviewModelView.cs
+++ OnlineConsultations.Data/Models/Review/DetailsReviewModelView.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.ReviewConstants;
 
 namespace OnlineConsultations.Data.Models.Review
 {
     public class DetailsReviewModelView
     {
+        [Key]
+        [Required]
+        public int ReviewId { get; set; }
+
+        [Required]
+        [StringLength(ReviewDescriptionMaxLength, MinimumLength = ReviewDescriptionMinLength)]
+        public required string ReviewDescription { get; set; }
+
+        [Required]
+        public DateTime ReviewDate { get; set; }
+
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public Entities.ProvideUser? ProvideUser { get; set; }
+
+        [Required]
+        public int SearchUserId { get; set; }
+
+        [ForeignKey(nameof(SearchUserId))]
+        public Entities.SearchUser? SearchUser { get; set; }
     }
 }
