56f81aee59204b2672063a8caaf90c81c255dc20
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-19 21:47:48 +0200

    Adde basic properties

--- OnlineConsultations.Data/Models/Rating/AddRatingModelView.cs
+++ OnlineConsultations.Data/Models/Rating/AddRatingModelView.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.RatingConstants;
 
 namespace OnlineConsultations.Data.Models.Rating
 {
     public class AddRatingModelView
     {
+        [Required]
+        public int RatingValue { get; set; }
+
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public Entities.ProvideUser? ProvideUser { get; set; }
+
+        [Required]
+        public int SearchUserId { get; set; }
+
+        [ForeignKey(nameof(SearchUserId))]
+        public Entities.SearchUser? SearchUser { get; set; }
     }
 }
--- OnlineConsultations.Data/Models/Rating/EditRatingModelView.cs
+++ OnlineConsultations.Data/Models/Rating/EditRatingModelView.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.RatingConstants;
 
 namespace OnlineConsultations.Data.Models.Rating
 {
     public class EditRatingModelView
     {
+        [Key]
+        [Required]
+        public int RatingId { get; set; }
+
+        [Required]
+        public int RatingValue { get; set; }
+
+        [Required]
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public Entities.ProvideUser? ProvideUser { get; set; }
+
+        [Required]
+        public int SearchUserId { get; set; }
+
+        [ForeignKey(nameof(SearchUserId))]
+        public Entities.SearchUser? SearchUser { get; set; }
     }
 }
