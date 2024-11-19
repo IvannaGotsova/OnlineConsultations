243179dc9a4fceb509f97fe967941774cfe02435
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-19 21:47:27 +0200

    Adde basic properties

--- OnlineConsultations.Data/Models/Message/AllMessagesModelView.cs
+++ OnlineConsultations.Data/Models/Message/AllMessagesModelView.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.MessageConstants;
 
 namespace OnlineConsultations.Data.Models.Message
 {
     public class AllMessagesModelView
     {
+        [Key]
+        [Required]
+        public int MessageId { get; set; }
+
+        [Required]
+        [StringLength(MessageDescriptionMaxLength, MinimumLength = MessageDescriptionMinLength)]
+        public required string MessageDescription { get; set; }
+
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public Entities.ProvideUser? ProvideUser { get; set; }
+
+        public int SearchUserId { get; set; }
+
+        [ForeignKey(nameof(SearchUserId))]
+        public Entities.SearchUser? SearchUser { get; set; }
+
     }
 }
--- OnlineConsultations.Data/Models/Message/EditMessageModelView.cs
+++ OnlineConsultations.Data/Models/Message/EditMessageModelView.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OnlineConsultations.Data.DataConstants.DataConstants.MessageConstants;
 
 namespace OnlineConsultations.Data.Models.Message
 {
     public class EditMessageModelView
     {
+        [Key]
+        [Required]
+        public int MessageId { get; set; }
+
+        [Required]
+        [StringLength(MessageDescriptionMaxLength, MinimumLength = MessageDescriptionMinLength)]
+        public required string MessageDescription { get; set; }
+
+        public int ProvideUserId { get; set; }
+
+        [ForeignKey(nameof(ProvideUserId))]
+        public Entities.ProvideUser? ProvideUser { get; set; }
+
+        public int SearchUserId { get; set; }
+
+        [ForeignKey(nameof(SearchUserId))]
+        public Entities.SearchUser? SearchUser { get; set; }
     }
 }
