d5ddf979939a1c22e0665315f0d1f7bbc9054825
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-20 21:49:14 +0200

    Implement basic methods

--- OnlineConsultations.Core/Services/AnswerService.cs
+++ OnlineConsultations.Core/Services/AnswerService.cs
@@ -1,6 +1,8 @@
-﻿using OnlineConsultations.Core.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineConsultations.Core.Contracts;
 using OnlineConsultations.Data.Entities;
 using OnlineConsultations.Data.Models.Answer;
+using OnlineConsultations.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,49 +13,145 @@ namespace OnlineConsultations.Core.Services
 {
     public class AnswerService : IAnswerService
     {
-        public Task Add(AddAnswerModelView addAnswerModel)
+        private readonly IRepository data;
+
+        public AnswerService(IRepository data)
         {
-            throw new NotImplementedException();
+            this.data = data;
         }
 
-        public Task Delete(int answerId)
+        public async Task Add(AddAnswerModelView addAnswerModel)
         {
-            throw new NotImplementedException();
+            var answerToBeAdded = new Answer()
+            {
+                AnswerDescription = addAnswerModel.AnswerDescription,
+                AnswerDate = addAnswerModel.AnswerDate,
+                QuestionId = addAnswerModel.QuestionId,
+                ProvideUserId = addAnswerModel.ProvideUserId
+            };
+
+            await this.data.AddAsync(answerToBeAdded);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task<DeleteAnswerModelView> DeleteAnswerForm(int answerId)
+        public async Task Delete(int answerId)
         {
-            throw new NotImplementedException();
+            await this.data.DeleteAsync<Answer>(answerId);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task Edit(int answerId, EditAnswerModelView editAnswerModel)
+        public async Task<DeleteAnswerModelView> DeleteAnswerForm(int answerId)
         {
-            throw new NotImplementedException();
+            var answerToBeDeleted = await
+                GetAnswerById(answerId);
+
+            var deleteAnswerModel = new DeleteAnswerModelView()
+            {
+                AnswerId = answerToBeDeleted.AnswerId,
+                AnswerDescription = answerToBeDeleted.AnswerDescription,
+                AnswerDate = answerToBeDeleted.AnswerDate, 
+                QuestionId = answerToBeDeleted.QuestionId,
+                ProvideUserId = answerToBeDeleted.ProvideUserId
+            };
+
+            return deleteAnswerModel;
         }
 
-        public Task<EditAnswerModelView> EditCreateForm(int answerId)
+        public async Task Edit(int answerId, EditAnswerModelView editAnswerModel)
         {
-            throw new NotImplementedException();
+            var answerToBeEdited = await
+                GetAnswerById(answerId);
+
+            answerToBeEdited.AnswerDescription = editAnswerModel.AnswerDescription;
+            answerToBeEdited.AnswerDate = editAnswerModel.AnswerDate;
+            answerToBeEdited.QuestionId = editAnswerModel.QuestionId;
+            answerToBeEdited.ProvideUserId = editAnswerModel.ProvideUserId;
+
+            this.data.Update<Answer>(answerToBeEdited);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<AllAnswersModelView>> GetAllAnswers()
+        public async Task<EditAnswerModelView> EditCreateForm(int answerId)
         {
-            throw new NotImplementedException();
+            var answerToBeEdited = await
+                GetAnswerById(answerId);
+
+            var editAnswerModel = new EditAnswerModelView()
+            {
+                AnswerDescription = answerToBeEdited.AnswerDescription,
+                AnswerDate = answerToBeEdited.AnswerDate,
+                QuestionId = answerToBeEdited.QuestionId,
+                ProvideUserId = answerToBeEdited.ProvideUserId,
+            };
+
+            return editAnswerModel;
         }
 
-        public Task<Answer> GetAnswerById(int answerId)
+        public async Task<IEnumerable<AllAnswersModelView>> GetAllAnswers()
         {
-            throw new NotImplementedException();
+            var answers = await data
+               .AllReadonly<Answer>()
+               .ToListAsync();
+
+            return answers
+                .Select(a => new AllAnswersModelView()
+                {
+                    AnswerId = a.AnswerId,
+                    AnswerDescription = a.AnswerDescription,
+                    AnswerDate = a.AnswerDate,
+                    QuestionId = a.QuestionId,
+                    ProvideUserId = a.ProvideUserId,
+                })
+                .ToList();
         }
 
-        public Task<DetailsAnswerModelView> GetAnswerDetailsById(int answerId)
+        public async Task<Answer> GetAnswerById(int answerId)
         {
-            throw new NotImplementedException();
+            var answer = await
+              this.data
+              .AllReadonly<Answer>()
+              .Where(a => a.AnswerId == answerId)
+              .FirstOrDefaultAsync();
+
+            if (answer == null)
+            {
+                throw new ArgumentNullException(null, nameof(answer));
+            }
+
+            return answer;
         }
 
-        public Task<IEnumerable<Answer>> GetAnswersForSelect()
+        public async Task<DetailsAnswerModelView> GetAnswerDetailsById(int answerId)
         {
-            throw new NotImplementedException();
+            var answer = await
+               this.data
+               .AllReadonly<Answer>()
+               .Include(a => a.Question)
+               .Include(a => a.ProvideUser)
+               .Where(a => a.AnswerId == answerId)
+               .Select(a => new DetailsAnswerModelView()
+               {
+                   AnswerId = a.AnswerId,
+                   AnswerDescription = a.AnswerDescription,
+                   AnswerDate = a.AnswerDate,
+                   QuestionId = a.QuestionId,
+                   ProvideUserId = a.ProvideUserId,
+               }).FirstOrDefaultAsync();
+
+            if (answer == null)
+            {
+                throw new ArgumentNullException(null, nameof(answer));
+            }
+
+            return answer;
+        }
+
+        public async Task<IEnumerable<Answer>> GetAnswersForSelect()
+        {
+            return await
+               this.data
+               .AllReadonly<Answer>()
+               .ToListAsync();
         }
     }
 }
