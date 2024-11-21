83b5edb4069b0bdea36a1d6ab371e186b21aaba9
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-21 20:46:14 +0200

    Implement basic methods

--- OnlineConsultations.Core/Services/CommentService.cs
+++ OnlineConsultations.Core/Services/CommentService.cs
@@ -1,6 +1,8 @@
-﻿using OnlineConsultations.Core.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineConsultations.Core.Contracts;
 using OnlineConsultations.Data.Entities;
 using OnlineConsultations.Data.Models.Comment;
+using OnlineConsultations.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,49 +13,150 @@ namespace OnlineConsultations.Core.Services
 {
     public class CommentService : ICommentService
     {
-        public Task Add(AddCommentModelView addCommentModel)
+        private readonly IRepository data;
+
+        public CommentService(IRepository data)
         {
-            throw new NotImplementedException();
+            this.data = data;
         }
 
-        public Task Delete(int commentId)
+        public async Task Add(AddCommentModelView addCommentModel)
         {
-            throw new NotImplementedException();
+            var commentToBeAdded = new Comment()
+            {
+                CommentTitle = addCommentModel.CommentTitle,
+                CommentDescription = addCommentModel.CommentDescription,
+                GuestUserId = addCommentModel.GuestUserId,
+                SearchUserId = addCommentModel.SearchUserId,
+                AnswerId = addCommentModel.AnswerId
+            };
+
+            await this.data.AddAsync(commentToBeAdded);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task<DeleteCommentModelView> DeleteCommentForm(int commentId)
+        public async Task Delete(int commentId)
         {
-            throw new NotImplementedException();
+            await this.data.DeleteAsync<Comment>(commentId);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task Edit(int commentId, EditCommentModelView editCommentModel)
+        public async Task<DeleteCommentModelView> DeleteCommentForm(int commentId)
         {
-            throw new NotImplementedException();
+            var commentToBeDeleted = await
+                GetCommentById(commentId);
+
+            var deleteCommentModel = new DeleteCommentModelView()
+            {
+                CommentId = commentToBeDeleted.CommentId,
+                CommentTitle = commentToBeDeleted.CommentTitle,
+                CommentDescription = commentToBeDeleted.CommentDescription,
+                GuestUserId = commentToBeDeleted.GuestUserId,
+                SearchUserId = commentToBeDeleted.SearchUserId,
+                AnswerId = commentToBeDeleted.AnswerId
+            };
+
+            return deleteCommentModel;
         }
 
-        public Task<EditCommentModelView> EditCreateForm(int commentId)
+        public async Task Edit(int commentId, EditCommentModelView editCommentModel)
         {
-            throw new NotImplementedException();
+            var commentToBeEdited = await
+                GetCommentById(commentId);
+
+            commentToBeEdited.CommentTitle = editCommentModel.CommentTitle;
+            commentToBeEdited.CommentDescription = editCommentModel.CommentDescription;
+            commentToBeEdited.GuestUserId = editCommentModel.GuestUserId;
+            commentToBeEdited.SearchUserId = editCommentModel.SearchUserId;
+            commentToBeEdited.AnswerId = editCommentModel.AnswerId;
+
+            this.data.Update<Comment>(commentToBeEdited);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<AllCommentsModelView>> GetAllComments()
+        public async Task<EditCommentModelView> EditCreateForm(int commentId)
         {
-            throw new NotImplementedException();
+            var commentToBeEdited = await
+                GetCommentById(commentId);
+
+            var editCommentModel = new EditCommentModelView()
+            {
+                CommentTitle = commentToBeEdited.CommentTitle,
+                CommentDescription = commentToBeEdited.CommentDescription,
+                GuestUserId = commentToBeEdited.GuestUserId,
+                SearchUserId = commentToBeEdited.SearchUserId,
+                AnswerId = commentToBeEdited.AnswerId
+            };
+
+            return editCommentModel;
         }
 
-        public Task<Comment> GetCommentById(int commentId)
+        public async Task<IEnumerable<AllCommentsModelView>> GetAllComments()
         {
-            throw new NotImplementedException();
+            var comments = await data
+               .AllReadonly<Comment>()
+               .ToListAsync();
+
+            return comments
+                .Select(c => new AllCommentsModelView()
+                {
+                    CommentTitle = c.CommentTitle,
+                    CommentDescription = c.CommentDescription,
+                    GuestUserId = c.GuestUserId,
+                    SearchUserId = c.SearchUserId,
+                    AnswerId = c.AnswerId
+                })
+                .ToList();
         }
 
-        public Task<DetailsCommentModelView> GetCommentDetailsById(int commentId)
+        public async Task<Comment> GetCommentById(int commentId)
         {
-            throw new NotImplementedException();
+            var comment = await
+              this.data
+              .AllReadonly<Comment>()
+              .Where(a => a.CommentId == commentId)
+              .FirstOrDefaultAsync();
+
+            if (comment == null)
+            {
+                throw new ArgumentNullException(null, nameof(comment));
+            }
+
+            return comment;
         }
 
-        public Task<IEnumerable<Comment>> GetCommentsForSelect()
+        public async Task<DetailsCommentModelView> GetCommentDetailsById(int commentId)
         {
-            throw new NotImplementedException();
+            var comment = await
+               this.data
+               .AllReadonly<Comment>()
+               .Include(c => c.Answer)
+               .Include(a => a.GuestUser)
+               .Include(a => a.SearchUser)
+               .Where(c => c.CommentId == commentId)
+               .Select(c => new DetailsCommentModelView()
+               {
+                   CommentTitle = c.CommentTitle,
+                   CommentDescription = c.CommentDescription,
+                   GuestUserId = c.GuestUserId,
+                   SearchUserId = c.SearchUserId,
+                   AnswerId = c.AnswerId
+               }).FirstOrDefaultAsync();
+
+            if (comment == null)
+            {
+                throw new ArgumentNullException(null, nameof(comment));
+            }
+
+            return comment;
+        }
+
+        public async Task<IEnumerable<Comment>> GetCommentsForSelect()
+        {
+            return await
+               this.data
+               .AllReadonly<Comment>()
+               .ToListAsync();
         }
     }
 }
