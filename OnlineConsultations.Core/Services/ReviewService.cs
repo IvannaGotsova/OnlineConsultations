27eea48ff18039c348781ff6fbefc694169dab4b
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-21 20:46:52 +0200

    Implement basic methods

--- OnlineConsultations.Core/Services/ReviewService.cs
+++ OnlineConsultations.Core/Services/ReviewService.cs
@@ -1,6 +1,10 @@
-﻿using OnlineConsultations.Core.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineConsultations.Core.Contracts;
 using OnlineConsultations.Data.Entities;
+using OnlineConsultations.Data.Models.Answer;
 using OnlineConsultations.Data.Models.Review;
+using OnlineConsultations.Data.Models.Review;
+using OnlineConsultations.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,49 +15,145 @@ namespace OnlineConsultations.Core.Services
 {
     public class ReviewService : IReviewService
     {
-        public Task Add(AddReviewModelView addReviewModel)
+        private readonly IRepository data;
+
+        public ReviewService(IRepository data)
         {
-            throw new NotImplementedException();
+            this.data = data;
         }
 
-        public Task Delete(int reviewId)
+        public async Task Add(AddReviewModelView addReviewModel)
         {
-            throw new NotImplementedException();
+            var reviewToBeAdded = new Review()
+            {
+                ReviewDescription = addReviewModel.ReviewDescription, 
+                ReviewDate = addReviewModel.ReviewDate,
+                SearchUserId = addReviewModel.SearchUserId,
+                ProvideUserId = addReviewModel.ProvideUserId
+            };
+
+            await this.data.AddAsync(reviewToBeAdded);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task<DeleteReviewModelView> DeleteReviewForm(int reviewId)
+        public async Task Delete(int reviewId)
         {
-            throw new NotImplementedException();
+            await this.data.DeleteAsync<Review>(reviewId);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task Edit(int reviewId, EditReviewModelView editReviewModel)
+        public async Task<DeleteReviewModelView> DeleteReviewForm(int reviewId)
         {
-            throw new NotImplementedException();
+            var reviewToBeDeleted = await
+                GetReviewById(reviewId);
+
+            var deleteReviewModel = new DeleteReviewModelView()
+            {
+                ReviewId = reviewToBeDeleted.ReviewId,
+                ReviewDescription = reviewToBeDeleted.ReviewDescription,
+                ReviewDate = reviewToBeDeleted.ReviewDate,
+                SearchUserId = reviewToBeDeleted.SearchUserId,
+                ProvideUserId = reviewToBeDeleted.ProvideUserId
+            };
+
+            return deleteReviewModel;
         }
 
-        public Task<EditReviewModelView> EditCreateForm(int reviewId)
+        public async Task Edit(int reviewId, EditReviewModelView editReviewModel)
         {
-            throw new NotImplementedException();
+            var reviewToBeEdited = await
+                GetReviewById(reviewId);
+
+            reviewToBeEdited.ReviewDescription = editReviewModel.ReviewDescription;
+            reviewToBeEdited.ReviewDate = editReviewModel.ReviewDate;
+            reviewToBeEdited.SearchUserId = editReviewModel.SearchUserId;
+            reviewToBeEdited.ProvideUserId = editReviewModel.ProvideUserId;
+
+            this.data.Update<Review>(reviewToBeEdited);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<AllReviewsModelView>> GetAllReviews()
+        public async Task<EditReviewModelView> EditCreateForm(int reviewId)
         {
-            throw new NotImplementedException();
+            var reviewToBeEdited = await
+                GetReviewById(reviewId);
+
+            var editReviewModel = new EditReviewModelView()
+            {
+                ReviewDescription = reviewToBeEdited.ReviewDescription,
+                ReviewDate = reviewToBeEdited.ReviewDate,
+                SearchUserId = reviewToBeEdited.SearchUserId,
+                ProvideUserId = reviewToBeEdited.ProvideUserId,
+            };
+
+            return editReviewModel;
         }
 
-        public Task<Review> GetReviewById(int reviewId)
+        public async Task<IEnumerable<AllReviewsModelView>> GetAllReviews()
         {
-            throw new NotImplementedException();
+            var reviews = await data
+               .AllReadonly<Review>()
+               .ToListAsync();
+
+            return reviews
+                .Select(r => new AllReviewsModelView()
+                {
+                    ReviewId = r.ReviewId,
+                    ReviewDescription = r.ReviewDescription,
+                    ReviewDate = r.ReviewDate,
+                    SearchUserId = r.SearchUserId,
+                    ProvideUserId = r.ProvideUserId,
+                })
+                .ToList();
         }
 
-        public Task<DetailsReviewModelView> GetReviewDetailsById(int reviewId)
+        public async Task<Review> GetReviewById(int reviewId)
         {
-            throw new NotImplementedException();
+            var review = await
+              this.data
+              .AllReadonly<Review>()
+              .Where(r => r.ReviewId == reviewId)
+              .FirstOrDefaultAsync();
+
+            if (review == null)
+            {
+                throw new ArgumentNullException(null, nameof(review));
+            }
+
+            return review;
         }
 
-        public Task<IEnumerable<Review>> GetReviewsForSelect()
+        public async Task<DetailsReviewModelView> GetReviewDetailsById(int reviewId)
         {
-            throw new NotImplementedException();
+            var review = await
+               this.data
+               .AllReadonly<Review>()
+               .Include(r => r.SearchUserId)
+               .Include(r => r.ProvideUserId)
+               .Where(r => r.ReviewId == reviewId)
+               .Select(r => new DetailsReviewModelView()
+               {
+                   ReviewId = r.ReviewId,
+                   ReviewDescription = r.ReviewDescription,
+                   ReviewDate = r.ReviewDate,
+                   SearchUserId = r.SearchUserId,
+                   ProvideUserId = r.ProvideUserId,
+               }).FirstOrDefaultAsync();
+
+            if (review == null)
+            {
+                throw new ArgumentNullException(null, nameof(review));
+            }
+
+            return review;
+        }
+
+        public async Task<IEnumerable<Review>> GetReviewsForSelect()
+        {
+            return await
+               this.data
+               .AllReadonly<Review>()
+               .ToListAsync();
         }
     }
 }
