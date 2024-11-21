1f266712fdbfbb9d9b7265c0dc978428ac98085b
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-21 20:46:24 +0200

    Implement basic methods

--- OnlineConsultations.Core/Services/MessageService.cs
+++ OnlineConsultations.Core/Services/MessageService.cs
@@ -1,6 +1,9 @@
-﻿using OnlineConsultations.Core.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineConsultations.Core.Contracts;
 using OnlineConsultations.Data.Entities;
 using OnlineConsultations.Data.Models.Message;
+using OnlineConsultations.Data.Models.Message;
+using OnlineConsultations.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,49 +14,139 @@ namespace OnlineConsultations.Core.Services
 {
     public class MessageService : IMessageService
     {
-        public Task Add(AddMessageModelView addMessageModel)
+        private readonly IRepository data;
+
+        public MessageService(IRepository data)
         {
-            throw new NotImplementedException();
+            this.data = data;
         }
 
-        public Task Delete(int messageId)
+        public async Task Add(AddMessageModelView addMessageModel)
         {
-            throw new NotImplementedException();
+            var messageToBeAdded = new Message()
+            {
+                MessageDescription = addMessageModel.MessageDescription,
+                SearchUserId = addMessageModel.SearchUserId,
+                ProvideUserId = addMessageModel.ProvideUserId
+            };
+
+            await this.data.AddAsync(messageToBeAdded);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task<DeleteMessageModelView> DeleteMessageForm(int messageId)
+        public async Task Delete(int messageId)
         {
-            throw new NotImplementedException();
+            await this.data.DeleteAsync<Message>(messageId);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task Edit(int messageId, EditMessageModelView editMessageModel)
+        public async Task<DeleteMessageModelView> DeleteMessageForm(int messageId)
         {
-            throw new NotImplementedException();
+            var messageToBeDeleted = await
+                GetMessageById(messageId);
+
+            var deleteMessageModel = new DeleteMessageModelView()
+            {
+                MessageId = messageToBeDeleted.MessageId,
+                MessageDescription = messageToBeDeleted.MessageDescription,
+                SearchUserId = messageToBeDeleted.SearchUserId,
+                ProvideUserId = messageToBeDeleted.ProvideUserId
+            };
+
+            return deleteMessageModel;
         }
 
-        public Task<EditMessageModelView> EditCreateForm(int messageId)
+        public async Task Edit(int messageId, EditMessageModelView editMessageModel)
         {
-            throw new NotImplementedException();
+            var messageToBeEdited = await
+                GetMessageById(messageId);
+
+            messageToBeEdited.MessageDescription = editMessageModel.MessageDescription;
+            messageToBeEdited.SearchUserId = editMessageModel.SearchUserId;
+            messageToBeEdited.ProvideUserId = editMessageModel.ProvideUserId;
+
+            this.data.Update<Message>(messageToBeEdited);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<AllMessagesModelView>> GetAllMessages()
+        public async Task<EditMessageModelView> EditCreateForm(int messageId)
         {
-            throw new NotImplementedException();
+            var messageToBeEdited = await
+                GetMessageById(messageId);
+
+            var editMessageModel = new EditMessageModelView()
+            {
+                MessageDescription = messageToBeEdited.MessageDescription,
+                SearchUserId = messageToBeEdited.SearchUserId,
+                ProvideUserId = messageToBeEdited.ProvideUserId,
+            };
+
+            return editMessageModel;
         }
 
-        public Task<Message> GetMessageById(int messageId)
+        public async Task<IEnumerable<AllMessagesModelView>> GetAllMessages()
         {
-            throw new NotImplementedException();
+            var messages = await data
+               .AllReadonly<Message>()
+               .ToListAsync();
+
+            return messages
+                .Select(m => new AllMessagesModelView()
+                {
+                    MessageId = m.MessageId,
+                    MessageDescription = m.MessageDescription,
+                    SearchUserId = m.SearchUserId,
+                    ProvideUserId = m.ProvideUserId,
+                })
+                .ToList();
         }
 
-        public Task<DetailsMessageModelView> GetMessageDetailsById(int messageId)
+        public async Task<Message> GetMessageById(int messageId)
         {
-            throw new NotImplementedException();
+            var message = await
+              this.data
+              .AllReadonly<Message>()
+              .Where(m => m.MessageId == messageId)
+              .FirstOrDefaultAsync();
+
+            if (message == null)
+            {
+                throw new ArgumentNullException(null, nameof(message));
+            }
+
+            return message;
         }
 
-        public Task<IEnumerable<Message>> GetMessagesForSelect()
+        public async Task<DetailsMessageModelView> GetMessageDetailsById(int messageId)
         {
-            throw new NotImplementedException();
+            var message = await
+               this.data
+               .AllReadonly<Message>()
+               .Include(m => m.SearchUser)
+               .Include(m => m.ProvideUser)
+               .Where(m => m.MessageId == messageId)
+               .Select(m => new DetailsMessageModelView()
+               {
+                   MessageId = m.MessageId,
+                   MessageDescription = m.MessageDescription,
+                   SearchUserId = m.SearchUserId,
+                   ProvideUserId = m.ProvideUserId,
+               }).FirstOrDefaultAsync();
+
+            if (message == null)
+            {
+                throw new ArgumentNullException(null, nameof(message));
+            }
+
+            return message;
+        }
+
+        public async Task<IEnumerable<Message>> GetMessagesForSelect()
+        {
+            return await
+               this.data
+               .AllReadonly<Message>()
+               .ToListAsync();
         }
     }
 }
