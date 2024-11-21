c86c3c2a44c8c54f33ebd54f1040684a04912919
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-21 20:46:47 +0200

    Implement basic methods

--- OnlineConsultations.Core/Services/RatingService.cs
+++ OnlineConsultations.Core/Services/RatingService.cs
@@ -1,6 +1,9 @@
-﻿using OnlineConsultations.Core.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineConsultations.Core.Contracts;
 using OnlineConsultations.Data.Entities;
 using OnlineConsultations.Data.Models.Rating;
+using OnlineConsultations.Data.Models.Rating;
+using OnlineConsultations.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,49 +14,139 @@ namespace OnlineConsultations.Core.Services
 {
     public class RatingService : IRatingService
     {
-        public Task Add(AddRatingModelView addRatingModel)
+        private readonly IRepository data;
+
+        public RatingService(IRepository data)
         {
-            throw new NotImplementedException();
+            this.data = data;
         }
 
-        public Task Delete(int ratingId)
+        public async Task Add(AddRatingModelView addRatingModel)
         {
-            throw new NotImplementedException();
+            var ratingToBeAdded = new Rating()
+            {
+                RatingValue = addRatingModel.RatingValue,
+                SearchUserId = addRatingModel.SearchUserId,
+                ProvideUserId = addRatingModel.ProvideUserId
+            };
+
+            await this.data.AddAsync(ratingToBeAdded);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task<DeleteRatingModelView> DeleteRatingForm(int ratingId)
+        public async Task Delete(int ratingId)
         {
-            throw new NotImplementedException();
+            await this.data.DeleteAsync<Rating>(ratingId);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task Edit(int ratingId, EditRatingModelView editRatingModel)
+        public async Task<DeleteRatingModelView> DeleteRatingForm(int ratingId)
         {
-            throw new NotImplementedException();
+            var ratingToBeDeleted = await
+                GetRatingById(ratingId);
+
+            var deleteRatingModel = new DeleteRatingModelView()
+            {
+                RatingId = ratingToBeDeleted.RatingId,
+                RatingValue = ratingToBeDeleted.RatingValue,
+                SearchUserId = ratingToBeDeleted.SearchUserId,
+                ProvideUserId = ratingToBeDeleted.ProvideUserId
+            };
+
+            return deleteRatingModel;
         }
 
-        public Task<EditRatingModelView> EditCreateForm(int ratingId)
+        public async Task Edit(int ratingId, EditRatingModelView editRatingModel)
         {
-            throw new NotImplementedException();
+            var ratingToBeEdited = await
+                GetRatingById(ratingId);
+
+            ratingToBeEdited.RatingValue = editRatingModel.RatingValue;
+            ratingToBeEdited.SearchUserId = editRatingModel.SearchUserId;
+            ratingToBeEdited.ProvideUserId = editRatingModel.ProvideUserId;
+
+            this.data.Update<Rating>(ratingToBeEdited);
+            await this.data.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<AllRatingsModelView>> GetAllRatings()
+        public async Task<EditRatingModelView> EditCreateForm(int ratingId)
         {
-            throw new NotImplementedException();
+            var ratingToBeEdited = await
+                GetRatingById(ratingId);
+
+            var editRatingModel = new EditRatingModelView()
+            {
+                RatingValue = ratingToBeEdited.RatingValue,
+                SearchUserId = ratingToBeEdited.SearchUserId,
+                ProvideUserId = ratingToBeEdited.ProvideUserId,
+            };
+
+            return editRatingModel;
         }
 
-        public Task<Rating> GetRatingById(int ratingId)
+        public async Task<IEnumerable<AllRatingsModelView>> GetAllRatings()
         {
-            throw new NotImplementedException();
+            var ratings = await data
+               .AllReadonly<Rating>()
+               .ToListAsync();
+
+            return ratings
+                .Select(r => new AllRatingsModelView()
+                {
+                    RatingId = r.RatingId,
+                    RatingValue = r.RatingValue,
+                    SearchUserId = r.SearchUserId,
+                    ProvideUserId = r.ProvideUserId,
+                })
+                .ToList();
         }
 
-        public Task<DetailsRatingModelView> GetRatingDetailsById(int ratingId)
+        public async Task<Rating> GetRatingById(int ratingId)
         {
-            throw new NotImplementedException();
+            var rating = await
+              this.data
+              .AllReadonly<Rating>()
+              .Where(r => r.RatingId == ratingId)
+              .FirstOrDefaultAsync();
+
+            if (rating == null)
+            {
+                throw new ArgumentNullException(null, nameof(rating));
+            }
+
+            return rating;
         }
 
-        public Task<IEnumerable<Rating>> GetRatingsForSelect()
+        public async Task<DetailsRatingModelView> GetRatingDetailsById(int ratingId)
         {
-            throw new NotImplementedException();
+            var rating = await
+               this.data
+               .AllReadonly<Rating>()
+               .Include(r => r.SearchUserId)
+               .Include(r => r.ProvideUserId)
+               .Where(r => r.RatingId == ratingId)
+               .Select(r => new DetailsRatingModelView()
+               {
+                   RatingId = r.RatingId,
+                   RatingValue = r.RatingValue,
+                   SearchUserId = r.SearchUserId,
+                   ProvideUserId = r.ProvideUserId,
+               }).FirstOrDefaultAsync();
+
+            if (rating == null)
+            {
+                throw new ArgumentNullException(null, nameof(rating));
+            }
+
+            return rating;
+        }
+
+        public async Task<IEnumerable<Rating>> GetRatingsForSelect()
+        {
+            return await
+               this.data
+               .AllReadonly<Rating>()
+               .ToListAsync();
         }
     }
 }
