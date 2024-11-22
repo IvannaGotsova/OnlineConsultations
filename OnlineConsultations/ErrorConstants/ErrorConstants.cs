45e16248b635a04dffc550c77cf0894eae989458
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-22 21:36:03 +0200

    Added error constants

---
+++ OnlineConsultations/ErrorConstants/ErrorConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineConsultations.ErrorConstants
+{
+    public static class ErrorConstants
+    {
+        public static class GlobalErrorConstants
+        {
+            public const string somethingWrong = "Something went wrong, please try again!";
+        }
+
+        public static class UserErrorConstants
+        {
+            public const string invalidLogin = "Invalid Login";
+            public const string unAvailableUserName = "This UserName is unavailable. Please try another one";
+            public const string unAvailableEmail = "This Email is unavailable. Please try another one";
+        }
+    }
+}
