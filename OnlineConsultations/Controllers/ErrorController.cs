214e31311b75a18e828768c137ac22ce195298ec
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-22 21:36:53 +0200

    Added basic methods

---
+++ OnlineConsultations/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineConsultations.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult PageNotFoundError()
+        {
+            return View();
+        }
+        public ActionResult UnauthorizedError()
+        {
+            return View();
+        }
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+        public ActionResult GenericError()
+        {
+            return View();
+        }
+    }
+}
