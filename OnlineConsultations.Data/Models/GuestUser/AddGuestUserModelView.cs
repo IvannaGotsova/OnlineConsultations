7aede3f418e9621f72b31abd7e7b12f8fd7f0862
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-26 19:55:35 +0200

    Modify model - removed required

--- OnlineConsultations.Data/Models/GuestUser/AddGuestUserModelView.cs
+++ OnlineConsultations.Data/Models/GuestUser/AddGuestUserModelView.cs
@@ -12,7 +12,7 @@ namespace OnlineConsultations.Data.Models.GuestUser
     public class AddGuestUserModelView
     {
         [Required]
-        public required string ApplicationUserId { get; set; }
+        public string ApplicationUserId { get; set; }
 
         [ForeignKey(nameof(ApplicationUserId))]
         public ApplicationUser? ApplicationUser { get; set; }
