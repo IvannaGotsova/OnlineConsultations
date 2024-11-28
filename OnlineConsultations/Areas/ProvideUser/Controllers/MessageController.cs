5c5fc703442a0806c79b74e8ae31531906b293ef
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-28 20:31:35 +0200

    Added basic controller

---
+++ OnlineConsultations/Areas/ProvideUser/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineConsultations.Areas.ProvideUser.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
