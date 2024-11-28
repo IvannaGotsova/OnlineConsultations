c9a089765a18c965685b6a36a1758b3e7ddc1cf0
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-28 20:31:41 +0200

    Added basic controller

---
+++ OnlineConsultations/Areas/ProvideUser/Controllers/PostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineConsultations.Areas.ProvideUser.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
