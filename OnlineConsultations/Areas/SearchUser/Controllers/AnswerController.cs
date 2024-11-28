f64f5787a683adc3ee3269a749cd7ea344eba966
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-28 20:32:35 +0200

    Added basic controller

---
+++ OnlineConsultations/Areas/SearchUser/Controllers/AnswerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineConsultations.Areas.SearchUser.Controllers
+{
+    public class AnswerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
