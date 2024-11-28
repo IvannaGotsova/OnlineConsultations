7975819649358778358f4bdfa282b8b7ed7f066a
IvannaGotsova <ivannagotsova@outlook.com>
2024-11-28 20:32:48 +0200

    Added basic controller

---
+++ OnlineConsultations/Areas/SearchUser/Controllers/GuestUserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineConsultations.Areas.SearchUser.Controllers
+{
+    public class GuestUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
