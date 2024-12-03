3934c1d40d64d031a6ece0ebf8ae38c5c2af0321
IvannaGotsova <ivannagotsova@outlook.com>
2024-12-03 20:40:50 +0200

    Added controller

---
+++ OnlineConsultations/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineConsultations.Areas.ProvideUser.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
