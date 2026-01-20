86b63dfb10ab939fe44b0ba60322e752c1e53d4b
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-20 08:46:09 +0200

    Specified area for the controller

--- OnlineConsultations/Areas/ProvideUser/Controllers/HomeController.cs
+++ OnlineConsultations/Areas/ProvideUser/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 
 namespace OnlineConsultations.Areas.ProvideUser.Controllers
 {
+    [Area("ProvideUser")]
     public class HomeController : Controller
     {
         public IActionResult Index()
--- OnlineConsultations/Areas/SearchUser/Controllers/HomeController.cs
+++ OnlineConsultations/Areas/SearchUser/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 
 namespace OnlineConsultations.Areas.SearchUser.Controllers
 {
+    [Area("SearchUser")]
     public class HomeController : Controller
     {
         public IActionResult Index()
