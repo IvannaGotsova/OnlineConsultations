9cda5253cdc9f349d235d8cd68cedcfc1ebbbc45
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-26 20:35:07 +0200

    Added ApplicationUser model

---
+++ OnlineConsultations.Data/Models/ApplicationUserModels/AddApplicationUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static OnlineConsultations.Data.DataConstants.DataConstants.ApplicationUserConstants;
+
+namespace SmoothieShop.Data.Models.ApplicationUserModels
+{
+    public class AddApplicationUserModel
+    {
+        [Required]
+        [StringLength(UserMaxLengthUsername, MinimumLength = UserMinLengthUsername)]
+        public string UserName { get; set; } = null!;
+        [Required]
+        [StringLength(UserMaxLengthFirstName, MinimumLength = UserMinLengthFirstName)]
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(UserMaxLengthLastName, MinimumLength = UserMinLengthLastName)]
+        public string? LastName { get; set; }
+    }
+}
