9585901434ab24751b7c32bf5eccd4dc5ff6c0cd
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-27 17:48:55 +0200

    Modified Interfece for IApplicationUser

--- OnlineConsultations.Core/Contracts/IApplicationUserService.cs
+++ OnlineConsultations.Core/Contracts/IApplicationUserService.cs
@@ -1,5 +1,6 @@
 ﻿using OnlineConsultations.Data.Entities;
 using OnlineConsultations.Data.Models.ApplicationUserModels;
+using SmoothieShop.Data.Models.ApplicationUserModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,12 @@ namespace OnlineConsultations.Core.Contracts
 {
     public interface IApplicationUserService
     {
-        Task<IEnumerable<ApplicationUserModelView>> GetApplicationUsers();
-
+        Task<IEnumerable<AllApplicationUsersModel>> GetApplicationUsers();
         Task<ApplicationUser> GetApplicaionUserById(string userId);
-      
-        Task<ApplicationUserModelView> DeleteCreateForm(string userId);
-     
+        Task<ApplicationUserModel> DeleteCreateForm(string userId);
         Task Delete(string userId);
-       
         Task<IEnumerable<ApplicationUser>> GetApplicationUsersForSelect();
-
-        public int GetApplicationUsersCount();
+        Task<DetailsApplicationUserModel> GetApplicationUserDetailsById(string applicationUserId);
+        int Count();
     }
 }
