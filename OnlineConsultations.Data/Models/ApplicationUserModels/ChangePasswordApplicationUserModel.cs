c50f2dd76af1875537b102fb70724a691998ed6f
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-29 16:39:35 +0200

    Added new model

---
+++ OnlineConsultations.Data/Models/ApplicationUserModels/ChangePasswordApplicationUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static OnlineConsultations.Data.DataConstants.DataConstants.ApplicationUserConstants;
+
+namespace SmoothieShop.Data.Models.ApplicationUserModels
+{
+    public class ChangePasswordApplicationUserModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public string OldPassword { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
