e04899906d98604782d07bba3f88c05099583824
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-31 09:53:15 +0200

    Added new model

---
+++ OnlineConsultations.Data/Models/ApplicationUserModels/DetailsApplicationUserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static OnlineConsultations.Data.DataConstants.DataConstants.ApplicationUserConstants;
+
+namespace SmoothieShop.Data.Models.ApplicationUserModels
+{
+    public class DetailsApplicationUserModel
+    {
+        public string Id { get; set; } = null!;
+        [Required]
+        [StringLength(UserMaxLengthUsername, MinimumLength = UserMinLengthUsername)]
+        public string UserName { get; set; } = null!;
+        [Required]
+        [StringLength(UserMaxLengthFirstName, MinimumLength = UserMinLengthFirstName)]
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(UserMaxLengthLastName, MinimumLength = UserMinLengthLastName)]
+        public string? LastName { get; set; }
+    }
+}
