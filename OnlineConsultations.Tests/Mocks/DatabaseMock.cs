bfc3ec4f523008be27685dd708ad89ffac8f1e93
IvannaGotsova <ivannagotsova@outlook.com>
2026-02-07 23:26:46 +0200

    Database Mock - added Instance

--- OnlineConsultations.Tests/Mocks/DatabaseMock.cs
+++ OnlineConsultations.Tests/Mocks/DatabaseMock.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineConsultations.Data;
+using OnlineConsultations.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,19 @@ using System.Threading.Tasks;
 
 namespace OnlineConsultations.Tests.Mocks
 {
-    public class DatabaseMock
+    public static class DatabaseMock
     {
+        public static OnlineConsultationsDbContext Instance
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<OnlineConsultationsDbContext>()
+                    .UseInMemoryDatabase("OnlineConsultationsInnMemoryDb"
+                    + DateTime.Now.Ticks.ToString())
+                    .Options;
+
+                return new OnlineConsultationsDbContext(dbContextOptions, false);
+            }
+        }
     }
 }
